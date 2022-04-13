ce865be423bb0ce5c6e405ef0cfe589089347cd9
Mehtaab-Creator <mehtaabnadeem@gmail.com>
2022-04-13 13:28:51 +0100

    fixed that barrier towards api

--- app.py
+++ app.py
@@ -6,16 +6,26 @@ import pandas as pd
 from subprocess import call
 #import plotly for viz
 import plotly.express as px
+import time
 
 #input
 hashtag = st.text_input('Search for a hashtag here', value="")
 
+my_bar = st.progress(0)
+
+for percent_complete in range(100):
+     time.sleep(0.1)
+     my_bar.progress(percent_complete + 1)
+
 #button
+
 if st.button('Get Data'):
     #run get data function here
     st.write(hashtag)
     call(['python3', 'tiktokdata.py', hashtag])
+    st.success('This is a success message!')
     #load in existing data
+        
     df = pd.read_csv('hashtag_data.csv')
 
     #visulisation
--- tiktokdata.py
+++ tiktokdata.py
@@ -1,33 +1,36 @@
 #importing the tiktok python SDK
+from operator import index
 from TikTokApi import TikTokApi as tiktok 
 #for exporting data into a json format
 import json
+from urllib3 import proxy_from_url
+#importing data cleaning process .py 
+from helpers import process_results
+#import pandas to create dataframe
+import pandas as pd
+#import sys to extract cmd line arguments
+import sys
 
-#setting up the instance
-api = tiktok()
 
-#creating an empty lists for data to append too
-users = []
-trending = []
+def get_data(hashtag):
+    verifyFp = 'verify_l1unfse9_sgoech18_fWLJ_4iLb_98ks_N5jaXerVeWzW'
+    #setting up the instance
+    api = tiktok(custom_verify_fp=verifyFp)
 
-#finding users with a specific name
-for user in api.search.users("TheRock"):
-    print(user.as_dict)
-    users.append(user.as_dict)
-    #exporting the list into a json file
-    
+    #creating an empty lists for data to append too
+    trending = []
 
-#pulling out specific hashtag
-for video in api.hashtag(name='funny').videos():
-    print(video.as_dict)
-    trending.append(video.as_dict)
- 
-    
-with open('trending_export.json', 'w') as f:
-        json.dump(trending, f)
+    #pulling out specific hashtag
+    for video in api.hashtag(name=hashtag).videos():
+        print(video.as_dict)
+        trending.append(video.as_dict)
 
-with open('user_export.json', 'w') as f:
-        json.dump(users, f)
+    flattened_data = process_results(trending)
 
-#print(trending)
-#print(users)
+    #covereted processed data dict into a dataframe and then into a csv
+    df = pd.DataFrame.from_dict(flattened_data, orient='index')
+    df.to_csv('hashtag_data.csv',index=False)
+
+if __name__ == '__main__':
+    #sys.argv holds all the arguments when you run a python script
+    get_data(sys.argv[1])
